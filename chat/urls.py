03e97569c56a73576f5afa0dca0e95b43f5f0899
KRISHIL-cyber <krishil005@gmail.com>
2021-03-22 09:47:03 -0400

    Initial commit

--- chat/urls.py
+++ chat/urls.py
@@ -1,13 +1,10 @@
-from django.urls import path
+from django.urls import path, re_path
+from .views import index, room
 
-from chat.views import(
-	private_chat_room_view,
-	create_or_return_private_chat,
-)
 
 app_name = 'chat'
 
 urlpatterns = [
-	path('', private_chat_room_view, name='private-chat-room'),
-	path('create_or_return_private_chat/', create_or_return_private_chat, name='create-or-return-private-chat'),
+    path('', index, name='index'),
+    re_path(r'^(?P<room_name>[^/]+)/$', room, name='room'),
 ]
